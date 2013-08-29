373d95131192e37318dcfc9eb23a170a31eb024a
tomoakitomoaki <redkirihararei@yahoo.co.jp>
2013-08-29 12:40:32 +0900

    first time to commit for this file

--- game.rb
+++
@@ -1,8 +0,0 @@
-require 'dxruby'
-Window.loop do
-image = Image.load("ikaros.jpg")
-
-Window.draw(0, 0, image)
-
-
-end
--- game/game.rb
+++
@@ -1,8 +0,0 @@
-require 'dxruby'
-window.loop do
-image = Image.load("ikaros.jpg")
-
-Window.draw(0, 0, image)
-
-
-end
